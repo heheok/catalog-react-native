34f18ecd5af5c9fee9e6e724cf36876b4ae2b544
Harun Akgün <harun@fireflyon.com>
2024-04-08 10:18:59 +0300

    first try

--- CatalogUI/src/App.tsx
+++ CatalogUI/src/App.tsx
@@ -1,6 +1,6 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
 import {
   SafeAreaView,
   ScrollView,
@@ -8,16 +8,16 @@ import {
   StyleSheet,
   useColorScheme,
   View,
-} from 'react-native';
-import { PESDK } from 'react-native-photoeditorsdk';
-import { Configuration, VESDK } from 'react-native-videoeditorsdk';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-import { ExampleListItem } from './ExampleListItem';
+} from "react-native";
+import { PESDK } from "react-native-photoeditorsdk";
+import { Configuration, VESDK } from "react-native-videoeditorsdk";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import { ExampleListItem } from "./ExampleListItem";
 
 const Stack = createNativeStackNavigator();
 
 function HomeScreen() {
-  const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() === "dark";
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.white,
@@ -27,37 +27,40 @@ function HomeScreen() {
   // highlight-theme
   const configuration: Configuration = {
     // Activate one of your themes.
-    theme: 'light',
-    custom: {
-      // Provide all available themes.
-      themes: {
-        light: {
-          toolbarBackground: '#ffffff',
-          menuBackground: '#f7f7f7',
-          background: '#ebebeb',
+    tools: [Tool.TEXT, Tool.TEXT_DESIGN, Tool.STICKER, Tool.FILTER],
+    sticker: {
+      categories: [
+        {
+          identifier: "imgly_sticker_category_giphy",
+          provider: {
+            apiKey: "QK8AGf8z9LXa382LFU7cineftTVtwboP",
+          },
         },
+      ],
+    },
+    trim: {
+      maximumDuration: 15.0,
+      forceMode: ForceTrimMode.IF_NEEDED,
+    },
+    export: {
+      serialization: {
+        enabled: true,
+        exportType: SerializationExportType.FILE_URL,
+      },
+      video: {
+        format: VideoFormat.MP4,
+        codec: VideoCodec.HEVC,
+        bitRate: COMPRESSOR_VIDEO_BITRATE,
+        quality: 1,
       },
     },
   };
-  // highlight-theme
-
-  async function openPhotoEditor() {
-    try {
-      const result = await PESDK.openEditor(
-        require('../assets/pesdk/LA.jpg'),
-        configuration,
-      );
-      console.log(result?.image);
-    } catch (error) {
-      console.log(error);
-    }
-  }
 
   async function openVideoEditor() {
     try {
       const result = await VESDK.openEditor(
-        require('../assets/vesdk/Skater.mp4'),
-        configuration,
+        require("../assets/vesdk/Skater.mp4"),
+        configuration
       );
       console.log(result?.video);
     } catch (error) {
@@ -67,22 +70,26 @@ function HomeScreen() {
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        style={{...backgroundStyle, marginTop: 15}}>
+        style={{ ...backgroundStyle, marginTop: 15 }}
+      >
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
+          }}
+        >
           <ExampleListItem
             title="Open photo editor"
             description="Open the photo editor with user interface customizations."
-            onPress={openPhotoEditor}></ExampleListItem>
+            onPress={openPhotoEditor}
+          ></ExampleListItem>
           <ExampleListItem
             title="Open video editor"
             description="Open the video editor with user interface customizations."
-            onPress={openVideoEditor}></ExampleListItem>
+            onPress={openVideoEditor}
+          ></ExampleListItem>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -96,7 +103,7 @@ const App = () => {
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{title: 'IMG.LY for React Native'}}
+          options={{ title: "IMG.LY for React Native" }}
         />
       </Stack.Navigator>
     </NavigationContainer>
